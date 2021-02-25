89ffa8fdc7af5426b317f1b15dc103e3f8520530
Andréa Alencar <andreaflether@gmail.com>
2021-02-25 12:42:32 -0300

    Finalizando funcionalidade de level up, incluindo reset dos botões

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
   const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleSuccesfullChallenge() {
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleFailedChallenge() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -22,14 +34,14 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeFailedButton}
-              onClick={ resetChallenge }
+              onClick={ handleFailedChallenge }
             >
               Falhei
             </button>
             <button
               type="button"
               className={styles.challengeSucceededButton}
-              onClick={ completeChallenge }
+              onClick={ handleSuccesfullChallenge }
             >
               Completei
             </button>
